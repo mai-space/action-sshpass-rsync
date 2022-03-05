edbb8fec4eaae2b8dec179f817393beb090afa73
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-03-05 10:35:10 +0100

    [FEATURE] Add version v1
    Added private key option
    Added before deployment commands
    Added after deployment commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,60 @@
 set -e
 
 echo "#################################################"
-echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+echo "> Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 # Available env
 # echo "INPUT_HOST: ${INPUT_HOST}"
 # echo "INPUT_PORT: ${INPUT_PORT}"
 # echo "INPUT_USER: ${INPUT_USER}"
 # echo "INPUT_PASS: ${INPUT_PASS}"
+# echo "INPUT_KEY: ${INPUT_KEY}"
 # echo "INPUT_LOCAL: ${INPUT_LOCAL}"
 # echo "INPUT_REMOTE: ${INPUT_REMOTE}"
+# echo "INPUT_RUN_BEFORE: ${INPUT_RUNBEFORE}"
+# echo "INPUT_RUN_AFTER: ${INPUT_RUNAFTER}"
 
-echo "Exporting Password"
-export SSHPASS=$PASS
+RUNBEFORE="${INPUT_RUNBEFORE/$'\n'/' && '}"
+RUNAFTER="${INPUT_RUNBEFORE/$'\n'/' && '}"
 
-echo "Trying to connect"
-CMD="${INPUT_RUN/$'\n'/' && '}"
-sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$CMD"
+if [ -z "$INPUT_KEY" ]
+then # Password
+  echo "> Exporting Password"
+  export SSHPASS=$PASS
 
-echo "Deploying now"
-sshpass -p $INPUT_PASS rsync --progress -a -v -e "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
+  echo "> Executing commands before deployment"
+  sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$RUNBEFORE"
+
+  echo "> Deploying now"
+  sshpass -p $INPUT_PASS rsync --progress -a -v -e "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
+
+  echo "> Executing commands after deployment"
+  sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$RUNAFTER"
+else # Private key
+  pwd
+  mkdir "/root/.ssh"
+
+  echo "$INPUT_KEY" > "/root/.ssh/id_rsa"
+  chmod 400 "/root/.ssh/id_rsa"
+
+  echo "Host *" > "/root/.ssh/config"
+  echo "  AddKeysToAgent yes" >> "/root/.ssh/config"
+  echo "  IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
+
+  cat "/root/.ssh/config"
+
+  ls -lha "/root/.ssh/"
+
+  echo "> Executing commands before deployment"
+  sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$RUNBEFORE"
+
+  echo "> Deploying now"
+  sshpass -e rsync --progress -a -v -e "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
+
+  echo "> Executing commands after deployment"
+  sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$RUNAFTER"
+fi
+
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"

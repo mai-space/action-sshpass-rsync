957abbed84f78620526fc27b21934eb9fcb894b2
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-03-05 09:44:56 +0100

    [BUGFIX] Add version v0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,18 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 # Available env
-# echo "INPUT_HOST: ${INPUT_HOST}"
-# echo "INPUT_PORT: ${INPUT_PORT}"
-# echo "INPUT_USER: ${INPUT_USER}"
-# echo "INPUT_PASS: ${INPUT_PASS}"
-# echo "INPUT_LOCAL: ${INPUT_LOCAL}"
-# echo "INPUT_REMOTE: ${INPUT_REMOTE}"
+echo "INPUT_HOST: ${INPUT_HOST}"
+echo "INPUT_PORT: ${INPUT_PORT}"
+echo "INPUT_USER: ${INPUT_USER}"
+echo "INPUT_PASS: ${INPUT_PASS}"
+echo "INPUT_LOCAL: ${INPUT_LOCAL}"
+echo "INPUT_REMOTE: ${INPUT_REMOTE}"
 
 CMD="${INPUT_RUN/$'\n'/' && '}"
 
 export SSHPASS=$PASS
 
-sshpass -p $INPUT_PASS rsync -ae "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL  $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
+sshpass -p $INPUT_PASS rsync -ae "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"

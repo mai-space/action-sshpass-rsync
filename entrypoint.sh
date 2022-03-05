678dcead17a68decbc352f60d10daecc47fc672e
Joel Mai <38419872+Inf166@users.noreply.github.com>
2022-03-05 09:52:44 +0100

    [BUGFIX] Add version v0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,21 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 # Available env
-echo "INPUT_HOST: ${INPUT_HOST}"
-echo "INPUT_PORT: ${INPUT_PORT}"
-echo "INPUT_USER: ${INPUT_USER}"
-echo "INPUT_PASS: ${INPUT_PASS}"
-echo "INPUT_LOCAL: ${INPUT_LOCAL}"
-echo "INPUT_REMOTE: ${INPUT_REMOTE}"
+# echo "INPUT_HOST: ${INPUT_HOST}"
+# echo "INPUT_PORT: ${INPUT_PORT}"
+# echo "INPUT_USER: ${INPUT_USER}"
+# echo "INPUT_PASS: ${INPUT_PASS}"
+# echo "INPUT_LOCAL: ${INPUT_LOCAL}"
+# echo "INPUT_REMOTE: ${INPUT_REMOTE}"
 
-CMD="${INPUT_RUN/$'\n'/' && '}"
-
+echo "Exporting Password"
 export SSHPASS=$PASS
 
+echo "Trying to connect"
+CMD="${INPUT_RUN/$'\n'/' && '}"
+sshpass -e ssh -o StrictHostKeyChecking=no -p $INPUT_PORT $INPUT_USER@$INPUT_HOST "$CMD"
+
+echo "Deploying now"
 sshpass -p $INPUT_PASS rsync -ae "ssh -p $INPUT_PORT" $GITHUB_WORKSPACE/$INPUT_LOCAL $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
 
 echo "#################################################"
